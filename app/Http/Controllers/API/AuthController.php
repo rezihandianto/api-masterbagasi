b733ce18fcad5ad162b8584ae6e0cfc4f07995df
rezihandianto <rezihandianto98@gmail.com>
2024-10-02 04:03:30 +0700

    initial commit

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use App\Http\Requests\Auth\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(RegisterRequest $request)
+    {
+        $validatedData = $request->validated();
+
+        $user = User::create($validatedData);
+
+        return response()->json([
+            'message' => 'Registered successfully',
+            'data' => $user
+        ], 201);
+    }
+    public function login(LoginRequest $request)
+    {
+        try {
+            $validatedData = $request->validated();
+
+            if (!Auth::attempt($validatedData)) {
+                return response()->json([
+                    'message' => 'Invalid credentials'
+                ], 401);
+            }
+
+            $user = User::where('email', $validatedData['email'])->first();
+            $token = $user->createToken('auth_token')->plainTextToken;
+
+            return response()->json([
+                'message' => 'Logged in successfully',
+                'data' => [
+                    'access_token' => $token,
+                    'token_type' => 'Bearer',
+                    'user' => $user
+                ]
+            ], 200);
+        } catch (\Exception $ex) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'errors' => $ex->getMessage()
+            ], 500);
+        }
+    }
+    public function user()
+    {
+        $user = Auth::user();
+        return response()->json([
+            'data' => $user
+        ], 200);
+    }
+    public function logout()
+    {
+        try {
+            Auth::user()->currentAccessToken()->delete();
+            return response()->json([
+                'message' => 'Logged out successfully'
+            ], 204);
+        } catch (\Exception $ex) {
+            return response()->json([
+                'message' => 'Something went wrong',
+                'errors' => $ex->getMessage()
+            ], 500);
+        }
+    }
+}
---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException(response()->json([
+            'message' => 'Validation failed',
+            'errors' => $errors
+        ], 422));
+    }
+}
